75a50be49e26cc5fca69d29fb78cc5034a3194ce
Allan Weber <a.cassianoweber@gmail.com>
2020-08-10 18:55:43 +0200

    basic ci pipeline

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM adoptopenjdk/openjdk11:alpine-jre
+
+EXPOSE 8080
+
+ENV JAVA_OPTS  "\
+    -XX:+UnlockExperimentalVMOptions \
+    -Xmx512M"
+
+ADD target/candidates-career*.jar candidates-career.jar
+
+ENTRYPOINT ["sh","-c","java $JAVA_OPTS -jar candidates-career.jar"]
--- src/main/java/com/allanweber/candidatescareer/CandidatesCareerApplication.java
+++ src/main/java/com/allanweber/candidatescareer/CandidatesCareerApplication.java
@@ -2,10 +2,12 @@ package com.allanweber.candidatescareer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @EnableConfigurationProperties
-@SpringBootApplication
+@SpringBootApplication(exclude={MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class CandidatesCareerApplication {
 
 	public static void main(String[] args) {
