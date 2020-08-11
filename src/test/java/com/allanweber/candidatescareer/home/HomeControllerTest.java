6b0ba63059e80a5713ebe0d20c85f526dbdf9799
Allan Weber <a.cassianoweber@gmail.com>
2020-08-11 14:57:49 +0200

    basic ci pipeline (#1)

---
+++ src/test/java/com/allanweber/candidatescareer/home/HomeControllerTest.java
@@ -0,0 +1,33 @@
+package com.allanweber.candidatescareer.home;
+
+import com.allanweber.candidatescareer.configuration.AppConfiguration;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.springframework.http.HttpStatus.OK;
+
+@ExtendWith(SpringExtension.class)
+class HomeControllerTest {
+
+    @Mock
+    private AppConfiguration appConfiguration;
+
+    @InjectMocks
+    private HomeController homeController;
+
+    @Test
+    void check_property() {
+        when(appConfiguration.getSomeProperty()).thenReturn("message");
+
+        ResponseEntity<?> response = homeController.get();
+
+        assertEquals(OK, response.getStatusCode());
+        assertEquals("message", response.getBody());
+    }
+}
