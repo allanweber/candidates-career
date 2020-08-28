cac1b17455f9c8a01c8ebf8a0316d7733de8c93c
Allan Weber <a.cassianoweber@gmail.com>
2020-08-28 17:47:25 +0200

    Fix imports

--- src/main/java/com/allanweber/candidatescareer/domain/candidate/repository/CandidateRepository.java
+++ src/main/java/com/allanweber/candidatescareer/domain/candidate/repository/CandidateRepository.java
@@ -2,10 +2,8 @@ package com.allanweber.candidatescareer.domain.candidate.repository;
 
 import com.allanweber.candidatescareer.domain.user.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
